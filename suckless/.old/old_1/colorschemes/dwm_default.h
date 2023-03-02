10a051239d972112781ee73bafadc6571baeecdc
Dário Batista <dariolbat@hotmail.com>
2023-03-02 11:43:35 +0000

    fix

--- suckless/.old/old_1/colorschemes/dwm_default.h
+++
@@ -1,6 +0,0 @@
-static const char fg_sel[]          = "#EEEEEE";
-static const char fg_norm[]         = "#BBBBBB";
-static const char bg_sel[]          = "#005577";
-static const char bg_norm[]         = "#222222";
-static const char border_norm[]     = "#1E1E1E";
-static const char border_sel[]      = "#005577";
--- suckless/.old/old_1/colorschemes/onedark.h
+++
@@ -1,6 +0,0 @@
-static const char fg_sel[]          = "#3A3A3A";
-static const char fg_norm[]         = "#BBBBBB";
-static const char bg_sel[]          = "#8FBCEE";
-static const char bg_norm[]         = "#282C34";
-static const char border_norm[]     = "#1E1E1E";
-static const char border_sel[]      = "#E06B74";
